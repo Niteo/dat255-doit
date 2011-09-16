919bdbc40419f15cfa5bfd7c4674733a5ff59531
niteo1337@gmail.com <niteo1337@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-16 12:40:53 +0000

    More interfaces added and updated.

---
+++ trunk/doIT/src/se/chalmers/doit/core/IPriority.java
@@ -0,0 +1,7 @@
+package se.chalmers.doit.core;
+
+public interface IPriority extends Comparable<Integer>{
+
+	public byte getValue();
+
+}
---
+++ trunk/doIT/src/se/chalmers/doit/util/ISortingStrategy.java
@@ -0,0 +1,11 @@
+package se.chalmers.doit.util;
+
+import java.util.List;
+
+import se.chalmers.doit.core.ITask;
+
+public interface ISortingStrategy {
+
+	public List<ITask> sort();
+
+}
