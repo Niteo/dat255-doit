6e9e16eec31245229e71826e418166785f9bbb94
baxemyr@gmail.com <baxemyr@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-16 08:23:58 +0000

    Renamed Main.java to DefaultListView.java, and moved it to the correct package (presentation.activities.implementation). Also renamed the layout to match the new name.

--- doIT/src/se/chalmers/doit/presentation/activities/implementation/DefaultListView.java
+++ doIT/src/se/chalmers/doit/presentation/activities/implementation/DefaultListView.java
@@ -1,13 +1,14 @@
-package se.chalmers.doit;
+package se.chalmers.doit.presentation.activities.implementation;
 
+import se.chalmers.doit.R;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Main extends Activity {
+public class DefaultListView extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.defaultlistview);
     }
 }
