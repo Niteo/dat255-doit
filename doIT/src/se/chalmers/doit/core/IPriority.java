d8477410a717e83ad334c2a786f7d7d83ec3984f
robelirobban@gmail.com <robelirobban@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-16 13:33:43 +0000

    Added javadoc for IPriority and ITask.

--- doIT/src/se/chalmers/doit/core/IPriority.java
+++ doIT/src/se/chalmers/doit/core/IPriority.java
@@ -1,7 +1,13 @@
 package se.chalmers.doit.core;
 
+/**
+ * Interface representing a comparable priority.
+ * @author Kaufmann
+ */
 public interface IPriority extends Comparable<Integer>{
-
+	/**
+	 * Returns the priority value for the priority.
+	 * @return byte value [-128, 127] representing the priority of the task
+	 */
 	public byte getValue();
-
 }
--- doIT/src/se/chalmers/doit/core/ITask.java
+++ doIT/src/se/chalmers/doit/core/ITask.java
@@ -2,22 +2,58 @@ package se.chalmers.doit.core;
 
 import java.util.Date;
 
+/**
+ * An interface for immutable classes carrying properties of a task.
+ * @author Kaufmann
+ *
+ */
 public interface ITask {
 
+	/**
+	 * Returns the represented task's ID.
+	 * @return integer representing the ID of the task
+	 */
 	public int getID();
 
+	/**
+	 * Returns the represented task's name.
+	 * @return String representing the ID of the task
+	 */
 	public String getName();
-
+	
+	/**
+	 * Returns the represented task's description.
+	 * @return String representing the description of the task
+	 */
 	public String getDescription();
 
+	/**
+	 * Returns the represented task's due date.
+	 * @return date representing the due date of the task
+	 */
 	public Date getDueDate();
 
+	/**
+	 * Returns the represented task's priority.
+	 * @return IPriority representing the priority of the task
+	 */
 	public IPriority getPriority();
 
+	/**
+	 * Returns the represented task's reminder date.
+	 * @return String representing the reminder date of the task
+	 */
 	public Date getReminderDate();
 
+	/**
+	 * Returns the represented task's name.
+	 * @return String representing the ID of the task
+	 */
 	public int getCustomPosition();
 
+	/**
+	 * Returns true if the task is completed, else false.
+	 * @return boolean representing if the task is completed or not
+	 */
 	public boolean isCompleted();
-
 }
