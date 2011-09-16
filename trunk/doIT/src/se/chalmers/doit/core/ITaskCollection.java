ff18edf9a247fca033a70d55de23891c73a0b8e7
robelirobban@gmail.com <robelirobban@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-16 13:07:17 +0000

    Changed ITaskList to ITaskCollection and javadoced the interface.

---
+++ trunk/doIT/src/se/chalmers/doit/core/ITaskCollection.java
@@ -0,0 +1,22 @@
+package se.chalmers.doit.core;
+
+import java.util.Collection;
+
+/**
+ * Represents a list containing a collection of ID-numbers for tasks.
+ * @author Kaufmann
+ *
+ */
+public interface ITaskCollection {
+	/**
+	 * Returns the list's name.
+	 * @return a string representing the list's name
+	 */
+	public String getName();
+	
+	/**
+	 * Returns the collection of IDs the collection contains.
+	 * @return collection of IDs
+	 */
+	public Collection<Integer> getIDCollection();
+}
