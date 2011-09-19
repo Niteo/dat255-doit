1764f1cf9f86a0a1d3a5a2e2364c2a75e78ef325
baxemyr@gmail.com <baxemyr@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-19 14:45:01 +0000

    Implemented immutable class Task according to ITask.

---
+++ doIT/src/se/chalmers/doit/core/implementation/Task.java
@@ -0,0 +1,87 @@
+package se.chalmers.doit.core.implementation;
+
+import java.util.Date;
+
+import se.chalmers.doit.core.*;
+
+/**
+ * Immutable class carrying properties of a task.
+ * 
+ * @author Marco Baxemyr
+ *
+ */
+
+public final class Task implements ITask {
+	
+	final private int id;
+	final private String name;
+	final private String description;
+	final private IPriority priority;
+	final private Date dueDate;
+	final private Date reminderDate;
+	final private int customPosition;
+	final private boolean isCompleted;
+	
+	public Task(int id, String name, String description, IPriority priority, Date dueDate, Date reminderDate, int customPosition, boolean isCompleted) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.priority = priority;
+		this.dueDate = new Date(dueDate.getTime());
+		this.reminderDate = new Date(reminderDate.getTime());
+		this.customPosition = customPosition;
+		this.isCompleted = isCompleted;
+	}
+	public Task(ITask task) {
+		this.id = task.getID();
+		this.name = task.getName();
+		this.description = task.getDescription();
+		this.priority = task.getPriority();
+		this.dueDate = new Date(task.getDueDate().getTime());
+		this.reminderDate = new Date(task.getReminderDate().getTime());
+		this.customPosition = task.getCustomPosition();
+		this.isCompleted = task.isCompleted();
+}
+	                                                                                                                                                 
+	
+	@Override
+	public int getCustomPosition() {
+		return customPosition;
+	}
+
+	@Override
+	public String getDescription() {
+		return description;
+	}
+
+	@Override
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	@Override
+	public int getID() {
+		return id;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public IPriority getPriority() {
+		return priority;
+	}
+
+	@Override
+	public Date getReminderDate() {
+		return reminderDate;
+	}
+
+	@Override
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+
+}
