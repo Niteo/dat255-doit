2bc1c58cb264ba3f50dc8d238efeca5988d12db0
robelirobban@gmail.com <robelirobban@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-19 14:05:49 +0000

    ISortingStrategy and ITaskListUtility have been javadoced

--- doIT/src/se/chalmers/doit/util/ISortingStrategy.java
+++ doIT/src/se/chalmers/doit/util/ISortingStrategy.java
@@ -1,11 +1,17 @@
 package se.chalmers.doit.util;
 
 import java.util.List;
-
 import se.chalmers.doit.core.ITask;
 
+/**
+ * Strategy for sorting a list containing classes implementing ITask. 
+ * @author Kaufmann
+ */
 public interface ISortingStrategy {
-
-	public List<ITask> sort();
-
+	/**
+	 * Sorts the given list.
+	 * @param list - the list to sort
+	 * @return a sorted version of the input list
+	 */
+	public List<ITask> sort(List<ITask> list);
 }
