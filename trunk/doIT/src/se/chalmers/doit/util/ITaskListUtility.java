50ab936c347708bacbd21906dff09f99166ff864
robelirobban@gmail.com <robelirobban@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-19 14:05:49 +0000

    ISortingStrategy and ITaskListUtility have been javadoced

--- trunk/doIT/src/se/chalmers/doit/util/ITaskListUtility.java
+++ trunk/doIT/src/se/chalmers/doit/util/ITaskListUtility.java
@@ -4,8 +4,20 @@ import java.util.List;
 
 import se.chalmers.doit.core.ITask;
 
+/**
+ * Utility for doing operations on lists containing ITask objects.
+ * @author Kaufmann
+ */
 public interface ITaskListUtility {
 
+	/**
+	 * Sorts the provided task using primary, secondary and tertiary sorting strategies.
+	 * @param list - list to sort
+	 * @param primary - primary sorting strategy
+	 * @param secondary - secondary sorting strategy
+	 * @param tertiary - tertiary sorting strategy
+	 * @return a list in the provided order
+	 */
 	public List<ITask> sortTasks(List<ITask> list, ISortingStrategy primary,
 			ISortingStrategy secondary, ISortingStrategy tertiary);
 
