af2d4334fd73a9dbf24a9f054377365d2ed7fc6b
niteo1337@gmail.com <niteo1337@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-19 14:59:21 +0000

    Methods added in interfaces.

--- doIT/src/se/chalmers/doit/core/ITaskCollection.java
+++ doIT/src/se/chalmers/doit/core/ITaskCollection.java
@@ -4,18 +4,21 @@ import java.util.Collection;
 
 /**
  * Represents a list containing a collection of ID-numbers for tasks.
+ *
  * @author Kaufmann
  *
  */
 public interface ITaskCollection {
 	/**
 	 * Returns the list's name.
+	 *
 	 * @return a string representing the list's name
 	 */
 	public String getName();
-	
+
 	/**
 	 * Returns the collection of IDs the collection contains.
+	 *
 	 * @return collection of IDs
 	 */
 	public Collection<Integer> getIDCollection();
--- doIT/src/se/chalmers/doit/data/storage/IDataStorage.java
+++ doIT/src/se/chalmers/doit/data/storage/IDataStorage.java
@@ -1,5 +1,27 @@
 package se.chalmers.doit.data.storage;
 
+import java.util.Collection;
+
+import se.chalmers.doit.core.ITask;
+import se.chalmers.doit.core.ITaskCollection;
+
 public interface IDataStorage {
+
+	public void addList(ITaskCollection collection);
+
+	public void addTask(ITask task, ITaskCollection collection);
+
+	public void editList(ITaskCollection oldCollection,
+			ITaskCollection newCollection);
+
+	public void editTask(ITask oldTask, ITask newTask);
+
+	public Collection<ITaskCollection> getLists();
+
+	public void removeList(ITaskCollection collection);
+
+	public void removeTask(ITask task);
+
+	public void removeTasks(Collection<ITask> listOfTasksToRemove);
 
 }
