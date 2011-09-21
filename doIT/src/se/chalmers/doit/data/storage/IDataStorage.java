cce94775cf514df4cd5189be0205994a26839252
karl.bristav@gmail.com <karl.bristav@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-21 14:29:04 +0000

    Wrote the rest of the methods in the IDataStorage interface. Still needs javadoc.

--- doIT/src/se/chalmers/doit/data/storage/IDataStorage.java
+++ doIT/src/se/chalmers/doit/data/storage/IDataStorage.java
@@ -9,16 +9,28 @@ public interface IDataStorage {
 
 	public void addList(ITaskCollection collection);
 
+	public void addLists(Collection<ITaskCollection> collection);
+
 	public void addTask(ITask task, ITaskCollection collection);
+
+	public void addTasks(Collection<ITask> tasks, ITaskCollection collection);
+
+	public void clearData();
 
 	public void editList(ITaskCollection oldCollection,
 			ITaskCollection newCollection);
 
 	public void editTask(ITask oldTask, ITask newTask);
 
-	public Collection<ITaskCollection> getLists();
+	public Collection<ITaskCollection> getAllLists();
+
+	public Collection<ITask> getAllTasks();
+
+	public void moveTask(ITask task, ITaskCollection collection);
 
 	public void removeList(ITaskCollection collection);
+
+	public void removeLists(Collection<ITaskCollection> collection);
 
 	public void removeTask(ITask task);
 
