549904fe130ed4da2fe8b4d8271f6721fafd131d
karl.bristav@gmail.com <karl.bristav@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-21 15:14:04 +0000

    Added methods to the IDataVerifier interface. No javadoc.

--- trunk/doIT/src/se/chalmers/doit/logic/verification/IDataVerifier.java
+++ trunk/doIT/src/se/chalmers/doit/logic/verification/IDataVerifier.java
@@ -1,5 +1,12 @@
 package se.chalmers.doit.logic.verification;
 
+import se.chalmers.doit.core.ITask;
+import se.chalmers.doit.core.ITaskCollection;
+
 public interface IDataVerifier {
+
+	public ITaskCollection verifyList(ITaskCollection collection);
+
+	public ITask verifyTask(ITask task);
 
 }
