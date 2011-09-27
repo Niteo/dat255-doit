7dbef62fcf2143712b0bc640dc27e2d042410b47
niteo1337@gmail.com <niteo1337@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-27 15:23:35 +0000

    Priority implemented.

---
+++ trunk/doIT/src/se/chalmers/doit/core/implementation/Priority.java
@@ -0,0 +1,29 @@
+package se.chalmers.doit.core.implementation;
+
+import se.chalmers.doit.core.IPriority;
+
+public class Priority implements IPriority {
+
+	private byte value;
+
+	public Priority(byte value){
+		this.value = value;
+	}
+
+	@Override
+	public int compareTo(IPriority priority) {
+		if (value > priority.getValue()) {
+			return 1;
+		} else if (value < priority.getValue()) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+	@Override
+	public byte getValue() {
+		return value;
+	}
+
+}
