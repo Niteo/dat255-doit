f12bb904d7e39829ceef0fcf2270e4b6a186d9b8
niteo1337@gmail.com <niteo1337@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-27 15:23:35 +0000

    Priority implemented.

--- doIT/src/se/chalmers/doit/core/IPriority.java
+++ doIT/src/se/chalmers/doit/core/IPriority.java
@@ -4,7 +4,7 @@ package se.chalmers.doit.core;
  * Interface representing a immutable comparable priority.
  * @author Kaufmann
  */
-public interface IPriority extends Comparable<Integer>{
+public interface IPriority extends Comparable<IPriority>{
 	/**
 	 * Returns the priority value for the priority.
 	 * @return byte value [-128, 127] representing the priority of the task
