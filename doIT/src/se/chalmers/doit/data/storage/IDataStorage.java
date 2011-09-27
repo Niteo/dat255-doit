15bfbca985366cc40114647739620df65d468add
karl.bristav@gmail.com <karl.bristav@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-27 15:52:15 +0000

    IDataStorage javadoc written

--- doIT/src/se/chalmers/doit/data/storage/IDataStorage.java
+++ doIT/src/se/chalmers/doit/data/storage/IDataStorage.java
@@ -5,35 +5,131 @@ import java.util.Collection;
 import se.chalmers.doit.core.ITask;
 import se.chalmers.doit.core.ITaskCollection;
 
+/**
+ * An interface handling the data storage between the application and a generic
+ * data storage
+ * 
+ * @author Karl Bristav
+ * 
+ */
 public interface IDataStorage {
 
+	/**
+	 * Adds a List to the data storage
+	 * 
+	 * @param collection
+	 *            The ITaskCollection to add
+	 */
 	public void addList(ITaskCollection collection);
 
+	/**
+	 * Adds several lists to the data storage
+	 * 
+	 * @param collection
+	 *            A Collection containing the ITaskCollections to add
+	 */
 	public void addLists(Collection<ITaskCollection> collection);
 
+	/**
+	 * Adds a task to a list in the data storage
+	 * 
+	 * @param task
+	 *            The ITask to be added
+	 * @param collection
+	 *            The ITaskCollection to which the ITask is added
+	 */
 	public void addTask(ITask task, ITaskCollection collection);
 
+	/**
+	 * Adds several tasks to a list in the data storage
+	 * 
+	 * @param tasks
+	 *            a Collection of ITasks to be added
+	 * @param collection
+	 *            the ITaskCollection to which the ITasks is added
+	 */
 	public void addTasks(Collection<ITask> tasks, ITaskCollection collection);
 
+	/**
+	 * Clears the data storage of all stored data
+	 */
 	public void clearData();
 
+	/**
+	 * "Edits" a list by replacing it with a new list
+	 * 
+	 * @param oldCollection
+	 *            The ITaskCollection to be replaced
+	 * @param newCollection
+	 *            The ITaskCollection to replace the old one
+	 */
 	public void editList(ITaskCollection oldCollection,
 			ITaskCollection newCollection);
 
+	/**
+	 * "Edits" a task by replacing it with a new task
+	 * 
+	 * @param oldTask
+	 *            The ITask to be replaced
+	 * @param newTask
+	 *            The ITask to replace the old one
+	 */
 	public void editTask(ITask oldTask, ITask newTask);
 
+	/**
+	 * Returns a Collection of all the existing ITaskCollections
+	 * 
+	 * @return The Collection containing all existing ITaskCollections
+	 */
 	public Collection<ITaskCollection> getAllLists();
 
+	/**
+	 * Return a Collection of all the existing ITasks
+	 * 
+	 * @return The Collection containing all existing ITasks
+	 */
 	public Collection<ITask> getAllTasks();
 
+	/**
+	 * Moves a task from one list to another list
+	 * 
+	 * @param task
+	 *            The ITask to be moved
+	 * @param collection
+	 *            The ITaskCollection to which the ITask shall be moved
+	 */
 	public void moveTask(ITask task, ITaskCollection collection);
 
+	/**
+	 * Removes a list from the data storage
+	 * 
+	 * @param collection
+	 *            The ITaskCollection to be removed
+	 */
 	public void removeList(ITaskCollection collection);
 
+	/**
+	 * Removes several lists from the data storage
+	 * 
+	 * @param collection
+	 *            A Collection containing the ITaskCollections to be removed
+	 */
 	public void removeLists(Collection<ITaskCollection> collection);
 
+	/**
+	 * Remove a task from the data storage
+	 * 
+	 * @param task
+	 *            The ITask to be removed
+	 */
 	public void removeTask(ITask task);
 
+	/**
+	 * Remove several tasks from the data storage
+	 * 
+	 * @param listOfTasksToRemove
+	 *            A Collection containing the ITasks to be removed
+	 */
 	public void removeTasks(Collection<ITask> listOfTasksToRemove);
 
 }
