61160f2145828c14e2e71af9b75a9806aae7af51
baxemyr@gmail.com <baxemyr@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-28 16:04:23 +0000

    Added an EditText-field and an "Add"-button at the bottom of DefaultListView. This enables quick-adding new tasks, however they aren't actually added yet. Removed unnecessary casting and import in TaskListAdapter, and made it use ITask instead of Task.

--- trunk/doIT/src/se/chalmers/doit/presentation/activities/implementation/TaskListAdapter.java
+++ trunk/doIT/src/se/chalmers/doit/presentation/activities/implementation/TaskListAdapter.java
@@ -1,20 +1,19 @@
 package se.chalmers.doit.presentation.activities.implementation;
 
 import se.chalmers.doit.R;
-import se.chalmers.doit.core.implementation.Task;
+import se.chalmers.doit.core.ITask;
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-public class TaskListAdapter extends ArrayAdapter<Task> {
+public class TaskListAdapter extends ArrayAdapter<ITask> {
 	private final Activity context;
-	private final Task[] tasks;
+	private final ITask[] tasks;
 
-	public TaskListAdapter(Activity context, Task[] tasks) {
+	public TaskListAdapter(Activity context, ITask[] tasks) {
 		super(context, R.layout.task_list_item, tasks);
 		this.context = context;
 		this.tasks = tasks;
@@ -22,16 +21,16 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		View view = convertView;  
-        if (view == null) {  
-                LayoutInflater inflater = (LayoutInflater) context.getLayoutInflater();  
-                view = inflater.inflate(R.layout.task_list_item, null);  
-        }  
-        Task task = tasks[position];  
-        TextView topText = (TextView) view.findViewById(R.id.toptext);  
-        TextView bottomText = (TextView) view.findViewById(R.id.bottomtext);  
-        topText.setText(task.getName());  
-        bottomText.setText(task.getDescription());  
-        return view;  
+		View view = convertView;
+		if (view == null) {
+			LayoutInflater inflater = context.getLayoutInflater();
+			view = inflater.inflate(R.layout.task_list_item, null);
+		}
+		ITask task = tasks[position];
+		TextView topText = (TextView) view.findViewById(R.id.toptext);
+		TextView bottomText = (TextView) view.findViewById(R.id.bottomtext);
+		topText.setText(task.getName());
+		bottomText.setText(task.getDescription());
+		return view;
 	}
 }
