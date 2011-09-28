ca235c1a826cbdf2ed48fbc8e5529bdd9591624d
baxemyr@gmail.com <baxemyr@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-28 16:04:23 +0000

    Added an EditText-field and an "Add"-button at the bottom of DefaultListView. This enables quick-adding new tasks, however they aren't actually added yet. Removed unnecessary casting and import in TaskListAdapter, and made it use ITask instead of Task.

--- doIT/src/se/chalmers/doit/presentation/activities/implementation/DefaultListView.java
+++ doIT/src/se/chalmers/doit/presentation/activities/implementation/DefaultListView.java
@@ -1,9 +1,15 @@
 package se.chalmers.doit.presentation.activities.implementation;
 
+import se.chalmers.doit.R;
+import se.chalmers.doit.core.ITask;
 import se.chalmers.doit.core.implementation.Task;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -12,10 +18,34 @@ public class DefaultListView extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.defaultlistview);
-        Task[] tasks = new Task[] { new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!") };
+        
+        ITask[] tasks = new Task[] { new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!") };
 		TaskListAdapter adapter = new TaskListAdapter(this, tasks);
 		setListAdapter(adapter);
+		setContentView(R.layout.defaultlistview);
+		
+		final EditText edittext = (EditText) findViewById(R.id.quickaddedittext);
+		Button quickAddButton = (Button) findViewById(R.id.quickaddbutton);
+		quickAddButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(DefaultListView.this, edittext.getText(), Toast.LENGTH_SHORT).show();	
+			}
+		});
+		
+	    edittext.setOnKeyListener(new OnKeyListener() {
+	        public boolean onKey(View v, int keyCode, KeyEvent event) {
+	            // If the event is a key-down event on the "enter" button
+	            if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+	                (keyCode == KeyEvent.KEYCODE_ENTER)) {
+	              // Perform action on key press
+	              Toast.makeText(DefaultListView.this, edittext.getText(), Toast.LENGTH_SHORT).show();
+	              return true;
+	            }
+	            return false;
+	        }
+	    });
     }
     
     @Override
@@ -26,6 +56,5 @@ public class DefaultListView extends ListActivity {
 		String keyword = o.toString();
 		Toast.makeText(this, "You selected: " + keyword, Toast.LENGTH_SHORT)
 				.show();
-
 	}
 }
