b4830ce4758ba350c1346d87d631cac6236edfa1
karl.bristav@gmail.com <karl.bristav@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-28 13:49:47 +0000

    IDataVerifier javadoc added

--- trunk/doIT/src/se/chalmers/doit/logic/verification/IDataVerifier.java
+++ trunk/doIT/src/se/chalmers/doit/logic/verification/IDataVerifier.java
@@ -3,10 +3,31 @@ package se.chalmers.doit.logic.verification;
 import se.chalmers.doit.core.ITask;
 import se.chalmers.doit.core.ITaskCollection;
 
+/**
+ * An interface verifying that a list or a task is correctly filled in, and
+ * fixes errors if any are discovered
+ * 
+ * @author Karl
+ * 
+ */
 public interface IDataVerifier {
 
+	/**
+	 * Verifies the contents of a list
+	 * 
+	 * @param collection
+	 *            The ITaskCollection to be verified
+	 * @return The corrected ITaskCollection
+	 */
 	public ITaskCollection verifyList(ITaskCollection collection);
 
+	/**
+	 * Verifies the contents of a task
+	 * 
+	 * @param task
+	 *            The ITask to be verified
+	 * @return The corrected ITask
+	 */
 	public ITask verifyTask(ITask task);
 
 }
