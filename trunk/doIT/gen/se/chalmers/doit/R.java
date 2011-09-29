f0150b35888110ea2eebe48d9e53053ec2d81b67
robelirobban@gmail.com <robelirobban@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-29 15:34:42 +0000

    Configured the project to be able to run Android JUnit tests via emulator or device. I've also added a testclass "PriorityTest.java" which tests "Priority.java" successfully. Testclasses in the future shall use "PriorityTest.java" as a template. Finally, I did a cosmetic change in "Priority.java".

--- trunk/doIT/gen/se/chalmers/doit/R.java
+++ trunk/doIT/gen/se/chalmers/doit/R.java
@@ -13,11 +13,22 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int bottom_control_bar=0x7f050001;
+        public static final int bottomtext=0x7f050005;
+        public static final int quickaddbutton=0x7f050003;
+        public static final int quickaddedittext=0x7f050002;
+        public static final int top_control_bar=0x7f050000;
+        public static final int toptext=0x7f050004;
+    }
     public static final class layout {
         public static final int defaultlistview=0x7f030000;
+        public static final int task_list_item=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int empty_list=0x7f040003;
         public static final int hello=0x7f040000;
+        public static final int quick_add_hint=0x7f040002;
     }
 }
