aea6c2e517b9e98770d854e82fb3ef409a81589b
baxemyr@gmail.com <baxemyr@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-29 14:28:12 +0000

    Now it's possible to add new tasks by entering a name and clicking "Add" (or pressing Enter on your software keyboard). Dynamically adding an item to the ListViews' Adapter refused to work until I used an ArrayList<ITask> in TaskListAdapters' constructor (using an array as before didn't work, and my attempt at using our own TaskCollection class proved unsuccessful).

--- trunk/doIT/src/se/chalmers/doit/presentation/activities/implementation/DefaultListView.java
+++ trunk/doIT/src/se/chalmers/doit/presentation/activities/implementation/DefaultListView.java
@@ -1,5 +1,7 @@
 package se.chalmers.doit.presentation.activities.implementation;
 
+import java.util.ArrayList;
+
 import se.chalmers.doit.R;
 import se.chalmers.doit.core.ITask;
 import se.chalmers.doit.core.implementation.Task;
@@ -11,50 +13,54 @@ import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class DefaultListView extends ListActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        ITask[] tasks = new Task[] { new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!"), new Task("Wash the dishes", "Use the Yes washing-up liquid"), new Task("Do homework", "All chapters!") };
-		TaskListAdapter adapter = new TaskListAdapter(this, tasks);
+	/** Called when the activity is first created. */
+
+	private TaskListAdapter adapter;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		ArrayList<ITask> tasks = new ArrayList<ITask>();
+		tasks.add(new Task("Wash the dishes", "Use the Yes washing-up liquid"));
+		tasks.add(new Task("Do homework", "All chapters!"));
+		adapter = new TaskListAdapter(this, tasks);
 		setListAdapter(adapter);
 		setContentView(R.layout.defaultlistview);
-		
+
 		final EditText edittext = (EditText) findViewById(R.id.quickaddedittext);
 		Button quickAddButton = (Button) findViewById(R.id.quickaddbutton);
 		quickAddButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(DefaultListView.this, edittext.getText(), Toast.LENGTH_SHORT).show();	
+				adapter.add(new Task(edittext.getText().toString(),
+						"Preset description."));
 			}
 		});
-		
-	    edittext.setOnKeyListener(new OnKeyListener() {
-	        public boolean onKey(View v, int keyCode, KeyEvent event) {
-	            // If the event is a key-down event on the "enter" button
-	            if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-	                (keyCode == KeyEvent.KEYCODE_ENTER)) {
-	              // Perform action on key press
-	              Toast.makeText(DefaultListView.this, edittext.getText(), Toast.LENGTH_SHORT).show();
-	              return true;
-	            }
-	            return false;
-	        }
-	    });
-    }
-    
-    @Override
+
+		edittext.setOnKeyListener(new OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// If the event is a key-down event on the "enter" button
+				if ((event.getAction() == KeyEvent.ACTION_DOWN)
+						&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
+					// Perform action on key press
+					adapter.add(new Task(edittext.getText().toString(),
+							"Preset description."));
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+
+	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 		// Get the item that was clicked
 		Object o = this.getListAdapter().getItem(position);
 		String keyword = o.toString();
-		Toast.makeText(this, "You selected: " + keyword, Toast.LENGTH_SHORT)
-				.show();
 	}
 }
