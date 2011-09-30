f68c3e32fdf01804c8825cb86983182b8fdca21e
karl.bristav@gmail.com <karl.bristav@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-30 11:49:17 +0000

    Fixed a slight javadoc typo in ITaskListUtility and added a javadoc description in Constants

--- doIT/src/se/chalmers/doit/util/implementation/Constants.java
+++ doIT/src/se/chalmers/doit/util/implementation/Constants.java
@@ -1,5 +1,11 @@
 package se.chalmers.doit.util.implementation;
 
+/**
+ * A class containing various constants used throughout the application
+ * 
+ * @author Robert Kaufmann
+ * 
+ */
 public final class Constants {
 	public static final byte PRIORITY_VERYLOW = 1;
 	public static final byte PRIORITY_LOW = 2;
@@ -7,6 +13,6 @@ public final class Constants {
 	public static final byte PRIORITY_HIGH = 4;
 	public static final byte PRIORITY_VERYHIGH = 5;
 	public static final byte PRIORITY_DEFAULT = PRIORITY_MEDIUM;
-	
+
 	public static final int CUSTOMPOSITION_DEFAULT = 0;
 }
