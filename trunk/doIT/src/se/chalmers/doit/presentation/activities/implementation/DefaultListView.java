1a6ac96d6cccfa8076c7539f210ae6a40f01ba1d
baxemyr@gmail.com <baxemyr@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-30 09:53:29 +0000

    Added a ContextMenu when long-pressing a Task (compare to right-click on the desktop), with Mark as Complete, Edit and Delete menu items. Deleting works, the others aren't implemented.

--- trunk/doIT/src/se/chalmers/doit/presentation/activities/implementation/DefaultListView.java
+++ trunk/doIT/src/se/chalmers/doit/presentation/activities/implementation/DefaultListView.java
@@ -7,12 +7,19 @@ import se.chalmers.doit.core.ITask;
 import se.chalmers.doit.core.implementation.Task;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnKeyListener;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemLongClickListener;
 
 public class DefaultListView extends ListActivity {
 	/** Called when the activity is first created. */
@@ -22,21 +29,24 @@ public class DefaultListView extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		setContentView(R.layout.defaultlistview);
 		ArrayList<ITask> tasks = new ArrayList<ITask>();
 		tasks.add(new Task("Wash the dishes", "Use the Yes washing-up liquid"));
 		tasks.add(new Task("Do homework", "All chapters!"));
 		adapter = new TaskListAdapter(this, tasks);
-		setListAdapter(adapter);
-		setContentView(R.layout.defaultlistview);
 
+		setListAdapter(adapter);
+
+		ListView list = getListView();
+
+		registerForContextMenu(list);
 		final EditText edittext = (EditText) findViewById(R.id.quickaddedittext);
 		Button quickAddButton = (Button) findViewById(R.id.quickaddbutton);
 		quickAddButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				adapter.add(new Task(edittext.getText().toString(),
+				addTask(new Task(edittext.getText().toString(),
 						"Preset description."));
 			}
 		});
@@ -47,7 +57,7 @@ public class DefaultListView extends ListActivity {
 				if ((event.getAction() == KeyEvent.ACTION_DOWN)
 						&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
 					// Perform action on key press
-					adapter.add(new Task(edittext.getText().toString(),
+					addTask(new Task(edittext.getText().toString(),
 							"Preset description."));
 					return true;
 				}
@@ -62,5 +72,48 @@ public class DefaultListView extends ListActivity {
 		// Get the item that was clicked
 		Object o = this.getListAdapter().getItem(position);
 		String keyword = o.toString();
+		Toast.makeText(DefaultListView.this, "Clicked on: " + keyword,
+				Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		if (v.getId() == android.R.id.list) {
+			AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+			menu.setHeaderTitle(adapter.getItem(info.position).getName());
+			String[] menuItems = { "Mark as Complete", "Edit", "Delete" };
+			for (int i = 0; i < menuItems.length; i++) {
+				menu.add(Menu.NONE, i, i, menuItems[i]);
+			}
+		}
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item
+				.getMenuInfo();
+
+		if (item.getTitle() == "Delete") {
+			Toast.makeText(DefaultListView.this, "Deleted", Toast.LENGTH_SHORT)
+					.show();
+			deleteTask(adapter.getItem(info.position));
+		} else {
+			Toast.makeText(DefaultListView.this,
+					"Clicked on: " + item.getTitle(), Toast.LENGTH_SHORT)
+					.show();
+		}
+
+		return true;
+	}
+
+	public void addTask(ITask task) {
+		adapter.add(task);
+		Toast.makeText(DefaultListView.this, "Task added!", Toast.LENGTH_SHORT)
+				.show();
+	}
+
+	public void deleteTask(ITask task) {
+		adapter.remove(task);
 	}
 }
