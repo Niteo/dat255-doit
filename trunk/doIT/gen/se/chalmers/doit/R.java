178c6a78e26cad834c38687c24468ac0076634d4
robelirobban@gmail.com <robelirobban@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-30 12:10:54 +0000

    Packages implemented. Boel wanted me to do this commit!

--- trunk/doIT/gen/se/chalmers/doit/R.java
+++ trunk/doIT/gen/se/chalmers/doit/R.java
@@ -8,18 +8,21 @@
 package se.chalmers.doit;
 
 public final class R {
+    public static final class array {
+        public static final int context_menu_items=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int bottom_control_bar=0x7f050001;
-        public static final int bottomtext=0x7f050005;
-        public static final int quickaddbutton=0x7f050003;
-        public static final int quickaddedittext=0x7f050002;
-        public static final int top_control_bar=0x7f050000;
-        public static final int toptext=0x7f050004;
+        public static final int bottom_control_bar=0x7f060001;
+        public static final int bottomtext=0x7f060005;
+        public static final int quickaddbutton=0x7f060003;
+        public static final int quickaddedittext=0x7f060002;
+        public static final int top_control_bar=0x7f060000;
+        public static final int toptext=0x7f060004;
     }
     public static final class layout {
         public static final int defaultlistview=0x7f030000;
