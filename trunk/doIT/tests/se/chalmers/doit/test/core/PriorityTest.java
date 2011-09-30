010c1d55913516adfacd08e4fb78c82a891fcef4
robelirobban@gmail.com <robelirobban@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-09-30 11:04:50 +0000

    Corrected assert to assertTrue. Also found a bug in Task (null-pointer exception in the copy constructor). Fixed it!

--- trunk/doIT/tests/se/chalmers/doit/test/core/PriorityTest.java
+++ trunk/doIT/tests/se/chalmers/doit/test/core/PriorityTest.java
@@ -7,42 +7,39 @@ import android.test.AndroidTestCase;
 
 /**
  * JUnit testclass for testing se.chalmers.doit.test.core.Task
+ * 
  * @author Kaufmann
  */
-public class PriorityTest extends AndroidTestCase{
+public class PriorityTest extends AndroidTestCase {
 	private Priority lowPriority;
 	private Priority mediumPriority;
 	private Priority highPriority;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		super.setUp();
-		lowPriority = new Priority((byte)0);
-		mediumPriority = new Priority((byte)3);
-		highPriority = new Priority((byte)5);
+		lowPriority = new Priority((byte) 0);
+		mediumPriority = new Priority((byte) 3);
+		highPriority = new Priority((byte) 5);
 	}
-	
-	public void testGetValue() throws Exception {
-		assert(lowPriority.getValue() == 0);
-		assert(mediumPriority.getValue() == 3);
-		assert(highPriority.getValue() == 5);
-		
-		assert(!(lowPriority.getValue() != 0));
-		assert(!(lowPriority.getValue() != 3));
-		assert(!(lowPriority.getValue() != 5));
+
+	public void testGetValue() {
+		assertTrue(lowPriority.getValue() == 0);
+		assertTrue(mediumPriority.getValue() == 3);
+		assertTrue(highPriority.getValue() == 5);
 	}
-	
-	public void testCompareTo() throws Exception {
-		assert(lowPriority.compareTo(lowPriority) == 0);
-		assert(lowPriority.compareTo(mediumPriority) == -1);
-		assert(lowPriority.compareTo(highPriority) == -1);
-		
-		assert(mediumPriority.compareTo(lowPriority) == 1);
-		assert(mediumPriority.compareTo(mediumPriority) == 0);
-		assert(mediumPriority.compareTo(highPriority) == -1);
-		
-		assert(highPriority.compareTo(lowPriority) == 1);
-		assert(highPriority.compareTo(mediumPriority) == 1);
-		assert(highPriority.compareTo(highPriority) == 0);
+
+	public void testCompareTo() {
+		assertTrue(lowPriority.compareTo(lowPriority) == 0);
+		assertTrue(lowPriority.compareTo(mediumPriority) == -1);
+		assertTrue(lowPriority.compareTo(highPriority) == -1);
+
+		assertTrue(mediumPriority.compareTo(lowPriority) == 1);
+		assertTrue(mediumPriority.compareTo(mediumPriority) == 0);
+		assertTrue(mediumPriority.compareTo(highPriority) == -1);
+
+		assertTrue(highPriority.compareTo(lowPriority) == 1);
+		assertTrue(highPriority.compareTo(mediumPriority) == 1);
+		assertTrue(highPriority.compareTo(highPriority) == 0);
 	}
 }
