e2f4c1dcc970cc7dfa55017467c246ef8d43601c
niteo1337@gmail.com <niteo1337@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-10-03 14:43:55 +0000

    IDataVerifier now returns booleans.

--- trunk/doIT/src/se/chalmers/doit/logic/verification/IDataVerifier.java
+++ trunk/doIT/src/se/chalmers/doit/logic/verification/IDataVerifier.java
@@ -6,28 +6,28 @@ import se.chalmers.doit.core.ITaskCollection;
 /**
  * An interface verifying that a list or a task is correctly filled in, and
  * fixes errors if any are discovered
- * 
+ *
  * @author Karl
- * 
+ *
  */
 public interface IDataVerifier {
 
 	/**
 	 * Verifies the contents of a list
-	 * 
+	 *
 	 * @param collection
 	 *            The ITaskCollection to be verified
-	 * @return The corrected ITaskCollection
+	 * @return A boolean stating if the list passed the verification or not
 	 */
-	public ITaskCollection verifyList(ITaskCollection collection);
+	public boolean verifyList(ITaskCollection collection);
 
 	/**
 	 * Verifies the contents of a task
-	 * 
+	 *
 	 * @param task
 	 *            The ITask to be verified
-	 * @return The corrected ITask
+	 * @return A boolean stating if the task passed the verification or not
 	 */
-	public ITask verifyTask(ITask task);
+	public boolean verifyTask(ITask task);
 
 }
