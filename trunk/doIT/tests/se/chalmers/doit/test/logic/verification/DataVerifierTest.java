fcfacbb5553b43f20721890937feca965f713848
karl.bristav@gmail.com <karl.bristav@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-10-04 16:07:24 +0000

    Added a check in DefaultListView so no task can be added without a name, implemented two new test classes and implemented a test for a new method in PriorityTest

---
+++ trunk/doIT/tests/se/chalmers/doit/test/logic/verification/DataVerifierTest.java
@@ -0,0 +1,52 @@
+package se.chalmers.doit.test.logic.verification;
+
+import org.junit.Before;
+
+import se.chalmers.doit.core.ITask;
+import se.chalmers.doit.core.implementation.Task;
+import se.chalmers.doit.logic.verification.IDataVerifier;
+import se.chalmers.doit.logic.verification.implementation.DataVerifier;
+import android.test.AndroidTestCase;
+
+public class DataVerifierTest extends AndroidTestCase {
+
+	ITask shortTask;
+	ITask longTask;
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+
+		String longString = "";
+		String shortString = "";
+
+		for (int i = 0; i < 32; i++) {
+			longString += "a";
+			shortString += "a";
+		}
+
+		longString += "a";
+
+		shortTask = new Task(shortString, "", false);
+		longTask = new Task(longString, "", false);
+	}
+
+	public void testDataVerifier() {
+		new DataVerifier();
+	}
+
+	public void testVerifyList() {
+		// TODO: implement after the proper method is implemented
+	}
+
+	public void testVerifyTask() {
+
+		final IDataVerifier dataVerifier = new DataVerifier();
+
+		assertTrue(dataVerifier.verifyTask(shortTask));
+
+		assertFalse(dataVerifier.verifyTask(longTask));
+
+	}
+
+}
