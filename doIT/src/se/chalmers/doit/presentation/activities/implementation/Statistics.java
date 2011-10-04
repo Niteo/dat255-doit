cecfa6b4d6f27599c0ab1c737df5b1dcc5c54c6b
robelirobban@gmail.com <robelirobban@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-10-04 15:07:27 +0000

    Added statistics view. Added a map between menu button ID and intents for coding pleasure. Threw some stuff into the about thingie as well.

---
+++ doIT/src/se/chalmers/doit/presentation/activities/implementation/Statistics.java
@@ -0,0 +1,13 @@
+package se.chalmers.doit.presentation.activities.implementation;
+
+import se.chalmers.doit.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Statistics extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.statistics);
+	}
+}
