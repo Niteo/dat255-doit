b61b340b3c4f46bf242b9369e1139fc50b8160cc
robelirobban@gmail.com <robelirobban@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-10-07 15:18:02 +0000

    Added deleting tasks/lists and deleting statistics (including confirmation dialog) from settings. Also made taskview and listview to autoupdate when gained focus.

--- doIT/src/se/chalmers/doit/presentation/activities/implementation/MainActivity.java
+++ doIT/src/se/chalmers/doit/presentation/activities/implementation/MainActivity.java
@@ -10,14 +10,8 @@ import android.widget.TabHost.OnTabChangeListener;
 
 public class MainActivity extends TabActivity {
 
-	
-	public void setActiveList(String listName){
-		((TaskViewer)getLocalActivityManager().getActivity("taskview")).setActiveList(listName);
-		getTabHost().setCurrentTab(0);
-	}
-	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mainview);
 		Resources res = getResources();
@@ -27,30 +21,42 @@ public class MainActivity extends TabActivity {
 
 		// Create an Intent to launch an Activity for the tab (to be reused)
 		intent = new Intent().setClass(this, TaskViewer.class);
-		spec = tabHost.newTabSpec("taskview")
+		spec = tabHost
+				.newTabSpec("taskview")
 				.setIndicator("Tasks", res.getDrawable(R.drawable.ic_menu_mark))
 				.setContent(intent);
 		tabHost.addTab(spec); // Do the same for the other tabs
-		
+
 		intent = new Intent().setClass(this, ListViewer.class);
-		spec = tabHost.newTabSpec("listview")
-				.setIndicator("Lists", res.getDrawable(R.drawable.ic_menu_agenda))
+		spec = tabHost
+				.newTabSpec("listview")
+				.setIndicator("Lists",
+						res.getDrawable(R.drawable.ic_menu_agenda))
 				.setContent(intent);
 		tabHost.addTab(spec);
 
 		tabHost.setCurrentTab(0);
-		
+
 		// Update a tab when changed to
 		OnTabChangeListener tcl = new OnTabChangeListener() {
 			@Override
-			public void onTabChanged(String tabId) {
-				if(tabId.equals("listview")){
-					((ListViewer)getLocalActivityManager().getActivity(tabId)).updateView();
-				} else if(tabId.equals("taskview")){
-					((TaskViewer)getLocalActivityManager().getActivity(tabId)).updateView();
-				}
+			public void onTabChanged(final String tabId) {
+				_updateTabs();
 			}
 		};
 		tabHost.setOnTabChangedListener(tcl);
+	}
+
+	public void setActiveList(final String listName) {
+		((TaskViewer) getLocalActivityManager().getActivity("taskview"))
+				.setActiveList(listName);
+		getTabHost().setCurrentTab(0);
+	}
+
+	private void _updateTabs() {
+		((ListViewer) getLocalActivityManager().getActivity("listview"))
+				.updateView();
+		((TaskViewer) getLocalActivityManager().getActivity("taskview"))
+				.updateView();
 	}
 }
