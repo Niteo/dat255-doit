9a7b9b621f6a51e3db5164f5b3eca9124593351b
baxemyr@gmail.com <baxemyr@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-10-11 17:13:10 +0000

    Created a autotest.sh bash script that currently checkouts the most recent revision from here, builds it with ant and attempts to install it on a Android device or emulator. Before running, change the SDKDIR-variable in autotest.sh to point to your android SDK path. Started working on making the script run the Android JUnit tests after installing, but our tests setup needs some work before that can work.

---
+++ doIT/autotest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#This script will fetch the most recent source code from Google Code, build it and attempt to install it on an Android device.
+
+SDKDIR="/home/phelerox/bin/android-sdk-linux_x86"
+rm -rf dat255-doit-read-only
+svn checkout http://dat255-doit.googlecode.com/svn/trunk/ dat255-doit-read-only
+cd dat255-doit-read-only/doIT
+
+echo "sdk.dir="$SDKDIR > local.properties
+ant debug
+$SDKDIR/platform-tools/adb install bin/MainActivity-debug.apk
+#TODO: Code for running tests
+#$SDKDIR/platform-tools/adb shell am instrument -w se.chalmers.doit.test.core/android.test.InstrumentationTestRunner
