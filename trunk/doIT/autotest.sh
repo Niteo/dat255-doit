a08739e310b4648d20940c6da1f870afb55c6c33
baxemyr@gmail.com <baxemyr@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-10-14 12:26:18 +0000

    Now changing dates in EditTaskView works as expected, except for Issue 3. This commit fixes Issue 4. Editing a completed task no longer marks the task as incomplete.

--- trunk/doIT/autotest.sh
+++ trunk/doIT/autotest.sh
@@ -3,12 +3,12 @@
 #This script will fetch the most recent source code from Google Code, build it and attempt to install it on an Android device.
 
 SDKDIR="/home/phelerox/bin/android-sdk-linux_x86"
-rm -rf dat255-doit-read-only
+#rm -rf dat255-doit-read-only
 svn checkout http://dat255-doit.googlecode.com/svn/trunk/ dat255-doit-read-only
 cd dat255-doit-read-only/doIT
 
 echo "sdk.dir="$SDKDIR > local.properties
 ant debug
-$SDKDIR/platform-tools/adb install bin/MainActivity-debug.apk
+$SDKDIR/platform-tools/adb install -r bin/MainActivity-debug.apk
 #TODO: Code for running tests
-#$SDKDIR/platform-tools/adb shell am instrument -w se.chalmers.doit.test.core/android.test.InstrumentationTestRunner
+$SDKDIR/platform-tools/adb shell am instrument -w -e package se.chalmers.doit se.chalmers.doit/android.test.InstrumentationTestRunner
