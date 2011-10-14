e3dbc66e4675dff675c7461839d15a86a11ce657
baxemyr@gmail.com <baxemyr@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-10-14 15:48:49 +0000

    Moved ours tests to a separate project (and folder). Added to trunk/. However, they also still exist in doIT/tests (should be deleted).

---
+++ trunk/doIT tests/src/se/chalmers/doit/test/core/PriorityTest.java
@@ -0,0 +1,52 @@
+package se.chalmers.doit.test.core;
+
+import se.chalmers.doit.core.implementation.Priority;
+import android.test.AndroidTestCase;
+
+/**
+ * JUnit testclass for testing se.chalmers.doit.test.core.Task
+ * 
+ * @author Kaufmann
+ */
+public class PriorityTest extends AndroidTestCase {
+	private Priority lowPriority;
+	private Priority mediumPriority;
+	private Priority highPriority;
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		lowPriority = new Priority((byte) 0);
+		mediumPriority = new Priority((byte) 3);
+		highPriority = new Priority((byte) 5);
+	}
+
+	public void testGetValue() {
+		assertTrue(lowPriority.getValue() == 0);
+		assertTrue(mediumPriority.getValue() == 3);
+		assertTrue(highPriority.getValue() == 5);
+	}
+
+	public void testCompareTo() {
+		assertTrue(lowPriority.compareTo(lowPriority) == 0);
+		assertTrue(lowPriority.compareTo(mediumPriority) == -1);
+		assertTrue(lowPriority.compareTo(highPriority) == -1);
+
+		assertTrue(mediumPriority.compareTo(lowPriority) == 1);
+		assertTrue(mediumPriority.compareTo(mediumPriority) == 0);
+		assertTrue(mediumPriority.compareTo(highPriority) == -1);
+
+		assertTrue(highPriority.compareTo(lowPriority) == 1);
+		assertTrue(highPriority.compareTo(mediumPriority) == 1);
+		assertTrue(highPriority.compareTo(highPriority) == 0);
+	}
+
+	public void testEquals() {
+		final Priority otherLowPriority = new Priority((byte) 0);
+
+		assertTrue(lowPriority.equals(lowPriority));
+		assertFalse(lowPriority.equals(highPriority));
+
+		assertTrue(lowPriority.equals(otherLowPriority));
+	}
+}
