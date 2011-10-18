fc69e0699cb701912c8c5ed0f70d82b3bb1f9433
niteo1337@gmail.com <niteo1337@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-10-18 20:36:12 +0000

    It is no longer possible to change a list's name to that of one that already exists. DataStorage now makes sure the same list/task can not be added more than once. TaskCollection now implements equals() and hashCode(). Fixed a bug in Task causing a nullpointer when comparing dates.

--- doIT/src/se/chalmers/doit/core/implementation/TaskCollection.java
+++ doIT/src/se/chalmers/doit/core/implementation/TaskCollection.java
@@ -11,12 +11,11 @@ public class TaskCollection implements ITaskCollection {
 	private String name;
 	private Collection<ITask> taskCollection;
 
-	
-	public TaskCollection (String name){
+	public TaskCollection(String name) {
 		this.name = name;
 		this.taskCollection = new ArrayList<ITask>();
 	}
-	
+
 	public TaskCollection(String name, Collection<ITask> taskCollection) {
 		this(name);
 		this.taskCollection = new ArrayList<ITask>(taskCollection);
@@ -30,5 +29,27 @@ public class TaskCollection implements ITaskCollection {
 	@Override
 	public Collection<ITask> getTasks() {
 		return new ArrayList<ITask>(taskCollection);
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (o instanceof ITaskCollection) {
+			ITaskCollection collection = (ITaskCollection) o;
+
+			if (collection.getName().equals(this.name)
+					&& collection.getTasks().size() == this.taskCollection
+							.size()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 101;
+		hash = hash * 31 + name.hashCode();
+		hash = hash * 19 + taskCollection.hashCode();
+		return hash;
 	}
 }
