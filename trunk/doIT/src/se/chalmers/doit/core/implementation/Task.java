bc9c5ec78973ecbc027f21f3bc1c96d52f9746ce
karl.bristav@gmail.com <karl.bristav@gmail.com@ac500317-bddc-3096-f3a9-f35b4b83082c>
2011-10-18 17:59:40 +0000

    Implemented equals() and hashCode() in Task

--- trunk/doIT/src/se/chalmers/doit/core/implementation/Task.java
+++ trunk/doIT/src/se/chalmers/doit/core/implementation/Task.java
@@ -2,18 +2,19 @@ package se.chalmers.doit.core.implementation;
 
 import java.util.Date;
 
-import se.chalmers.doit.core.*;
+import se.chalmers.doit.core.IPriority;
+import se.chalmers.doit.core.ITask;
 import se.chalmers.doit.util.implementation.Constants;
 
 /**
  * Immutable class carrying properties of a task.
  * 
  * @author Marco Baxemyr, Robert Kaufmann
- *
+ * 
  */
 
 public final class Task implements ITask {
-	
+
 	final private String name;
 	final private String description;
 	final private IPriority priority;
@@ -21,38 +22,47 @@ public final class Task implements ITask {
 	final private Date reminderDate;
 	final private int customPosition;
 	final private boolean isCompleted;
-	
-	public Task(String name, String description, IPriority priority, Date dueDate, Date reminderDate, int customPosition, boolean isCompleted) {
+
+	public Task(final String name, final String description,
+			final IPriority priority, final Date dueDate,
+			final Date reminderDate, final int customPosition,
+			final boolean isCompleted) {
 		this.name = name;
 		this.description = description;
 		this.priority = priority;
 		this.dueDate = dueDate == null ? null : new Date(dueDate.getTime());
-		this.reminderDate = reminderDate == null ? null : new Date(reminderDate.getTime());
+		this.reminderDate = reminderDate == null ? null : new Date(
+				reminderDate.getTime());
 		this.customPosition = customPosition;
 		this.isCompleted = isCompleted;
 	}
-	
-	public Task(ITask task) {
+
+	public Task(final ITask task) {
 		this.name = task.getName();
 		this.description = task.getDescription();
 		this.priority = task.getPriority();
-		this.dueDate = task.getDueDate() == null ? null : new Date(task.getDueDate().getTime());
-		this.reminderDate = task.getReminderDate() == null ? null : new Date(task.getReminderDate().getTime());
+		this.dueDate = task.getDueDate() == null ? null : new Date(task
+				.getDueDate().getTime());
+		this.reminderDate = task.getReminderDate() == null ? null : new Date(
+				task.getReminderDate().getTime());
 		this.customPosition = task.getCustomPosition();
 		this.isCompleted = task.isCompleted();
 	}
-	
-	public Task(ITask task, boolean completed) {
+
+	public Task(final ITask task, final boolean completed) {
 		this.name = task.getName();
 		this.description = task.getDescription();
 		this.priority = task.getPriority();
-		this.dueDate = task.getDueDate() == null ? null : new Date(task.getDueDate().getTime());
-		this.reminderDate = task.getReminderDate() == null ? null : new Date(task.getReminderDate().getTime());
+		this.dueDate = task.getDueDate() == null ? null : new Date(task
+				.getDueDate().getTime());
+		this.reminderDate = task.getReminderDate() == null ? null : new Date(
+				task.getReminderDate().getTime());
 		this.customPosition = task.getCustomPosition();
 		this.isCompleted = completed;
 	}
-	
-	public Task(String name, String description, boolean completed) {
+
+	public Task(final String name, final String description,
+			final boolean completed) {
 		this.name = name;
 		this.description = description;
 		this.priority = new Priority(Constants.PRIORITY_DEFAULT);
@@ -61,8 +71,7 @@ public final class Task implements ITask {
 		this.customPosition = Constants.CUSTOMPOSITION_DEFAULT;
 		this.isCompleted = completed;
 	}
-	                                                                                                                                                 
-	
+
 	@Override
 	public int getCustomPosition() {
 		return customPosition;
@@ -97,10 +106,45 @@ public final class Task implements ITask {
 	public boolean isCompleted() {
 		return isCompleted;
 	}
-	
+
 	@Override
 	public String toString() {
 		return name;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (o instanceof ITask) {
+			final ITask t = (ITask) o;
+
+			if (this.getCustomPosition() == t.getCustomPosition()
+					&& this.isCompleted() == t.isCompleted()
+					&& this.getDescription().equals(t.getDescription())
+					&& this.getDueDate().equals(t.getDueDate())
+					&& this.getName().equals(t.getName())
+					&& this.getPriority().getValue() == t.getPriority()
+							.getValue()
+					&& this.getReminderDate().equals(t.getReminderDate())) {
+				return true;
+			}
+			return false;
+		} else {
+			return false;
+		}
+
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 101;
+		hash = hash * 17 + customPosition;
+		hash = hash * 31 + name.hashCode();
+		hash = hash * 19 + (isCompleted ? 1 : 2);
+		hash = hash * 13 + (dueDate == null ? 0 : dueDate.hashCode());
+		hash = hash * 37 + (reminderDate == null ? 0 : reminderDate.hashCode());
+		hash = hash * 29 + priority.hashCode();
+		hash = hash * 23 + description.hashCode();
+		return hash;
 	}
 
 }
